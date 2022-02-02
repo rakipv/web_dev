94fba61e19a1d2d797cf878020d1e25ad45652c8
rakipv <96035992+rakipv@users.noreply.github.com>
2022-02-02 22:05:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,134 +1,138 @@
 
-if(window.XMLHttpRequest)
+var Name=document.getElementById('name');
+var email=document.getElementById('email');
+
+var phno=document.getElementById('phno');
+var address=document.getElementById('address');
+var span=document.getElementsByTagName('span');
+
+
+function myFunction() {
+  document.getElementById("form").reset();
+}
+
+
+
+
+    function validate(){
+        const re=/[1-9][0-9]{9}/;
+
+        const regex1= /^([a-z0-9\.-]+)@(christuniversity)\.(in)$/;
+        const regex2= /^[a-zA-Z]+$/
+        const regex3 = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])([a-z])([a-zA-Z0-9!@#$%^&*])(.*[a-z])?$/;
+        const isValidLength = /^.{8,16}$/;
+        //name validation
+        if(Name.value.trim().length<3){
+            $("#name").addClass("is-invalid")
+            $("#name").removeClass("is-valid")
+            span[1].innerText="Namelength should be greater than two";
+            span[1].className="invalid-feedback";
+            return false
+        }
+
+else if(!re.test(phno.value.trim())){ $("#phno").addClass("is-invalid")
+            $("#phno").removeClass("is-valid")
+        span[1].innerText=" phone number must contain only 10 didgits";
+            span[1].className="invalid-feedback";
+            return false
+    }
+
+        else if(!regex2.test(Name.value.trim())){
+            $("#name").addClass("is-invalid")
+            $("#name").removeClass("is-valid")
+            span[1].innerText="special characters are not allowed in Name";
+            span[1].className="invalid-feedback";
+            return false
+        }
+        else if(!regex1.test(email.value.trim())){
+            $("#email").addClass("is-invalid")
+                $("#email").removeClass("is-valid")
+                span[2].innerText="invalid email : re-enter according to format";
+                span[2].className="invalid-feedback";
+                return false
+        }
+       
+        else{
+            $("#name").addClass("is-valid")
+            $("#name").removeClass("is-invalid")
+            $("#email").addClass("is-valid")
+            $("#email").removeClass("is-invalid")
+            $("#phno").addClass("is-valid")
+            $("#phno").removeClass("is-invalid")
+            
+           
+            return true
+        }
+}
+
+email.onkeyup=function()
+    {
+        const regex1= /^([a-z0-9\.-]+)@(christuniversity)\.(in)$/;
+        if(regex1.test(email.value.trim()))
+        {
+
+                $("#email").addClass("is-valid")
+                $("#email").removeClass("is-invalid")
+                span[2].innerText="Email looking good";
+        }
+        else{
+                $("#email").addClass("is-invalid")
+                $("#email").removeClass("is-valid")
+                span[2].innerText="invalid email";
+        }
+    }
+phno.onkeyup=function(){
+
+
+
+const re=/[1-9][0-9]{9}/;
+if(re.test(phno.value.trim()))
+        {
+
+                $("#phno").addClass("is-valid")
+                $("#phno").removeClass("is-invalid")
+                span[2].innerText="valid Phone number";
+        }
+        else{
+                $("#phno").addClass("is-invalid")
+                $("#phno").removeClass("is-valid")
+                span[2].innerText="invalid Phone number";
+        }
+
+}
+Name.onkeyup=function(){
+    const regex2= /^[a-zA-Z]+$/;
+    if(Name.value.trim().length<3){
+        $("#name").addClass("is-invalid")
+        $("#name").removeClass("is-valid")
+        span[1].innerText="Name length should be greater than two";
+    }
+    else if(!regex2.test(Name.value.trim())){
+        $("#name").addClass("is-invalid")
+        $("#name").removeClass("is-valid")
+        span[1].innerText="special characters are not allowed in Name";
+    }
+    else{
+        $("#name").addClass("is-valid")
+        $("#name").removeClass("is-invalid")
+        span[1].innerText="Name looks good";
+    }
+}
+
+function numlock(event)
 {
-    xhttp=new XMLHttpRequest();
+    let code=event.which;
+    if(code>47&&code<58)
+    return false;
+    else
+    return true;
 }
-else{
-    xhttp=new ActiveXObject("Microsoft.XMLHTTP")
-}
-xhttp.open("GET","/emp.xml",false);
-xhttp.send();
-var xmlDoc=xhttp.responseXML;
 
 
-
-
-
-function display() {
-    var i;
-    var table =
-        `<thead class="thead-dark"><tr><th>Vehilce-Name</th><th>vehilce-Make</th><th>vehicle-Type</th>
-            <th>Gear-Type</th>
-        </tr></thead>`;
-        
-    var x = xmlDoc.getElementsByTagName("cd");
-    
-
-    // Start to fetch the data by using TagName 
-    for (i = 0; i < x.length; i++) {
-        table += "<tr><td>" +
-           x[i].getElementsByTagName("Car-Name")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Car-Make")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Car-Type")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Gear-Type")[0]      
-            .childNodes[0].nodeValue + "</td><td>"+
-            `<a onclick='javascript:remove(${i})'><i class='fas fa-trash'></i></a></td><td>`+
-            `<a onclick='javascript:change(${i})' data-toggle="modal" data-target="#new2"><i class="fas fa-user-edit"></i></a></td></tr>`;
+function phoneval(event){
+        let code=event.which;
+        if(code>64 && code<223){
+        return false;
+        }
+        else return True;
     }
-    // Print the xml data in table form
-    document.getElementById("id").innerHTML = table;
-}
-function remove(node){
-    y=xmlDoc.getElementsByTagName("cd")[node];
-    xmlDoc.documentElement.removeChild(y);
-    show(xmlDoc);
-    console.log(y);
-}
-
-function show(xmlDoc){
-    var i;
-    var table =
-        `<thead class="thead-dark"><tr><th>Vehilce-Name</th><th>vehilce-Make</th><th>vehicle-Type</th>
-            <th>Gear-Type</th>
-        </tr></thead>`;
-        
-    var x = xmlDoc.getElementsByTagName("cd");
-
-    // Start to fetch the data by using TagName 
-    for (i = 0; i < x.length; i++) {
-        table += "<tr><td>" +
-            x[i].getElementsByTagName("Car-Name")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Car-Make")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Car-Type")[0]
-            .childNodes[0].nodeValue + "</td><td>" +
-            x[i].getElementsByTagName("Gear-Type")[0]      
-            .childNodes[0].nodeValue + "</td><td>"+
-            `<a onclick='javascript:remove(${i})'><i class='fas fa-trash'></i></a></td><td>`+
-            `<a onclick='javascript:change(${i})' data-toggle="modal" data-target="#new2"><i class="fas fa-user-edit"></i></a></td></tr>`;
-    }
-    // Print the xml data in table form
-    document.getElementById("id").innerHTML = table;
-}
-
-function add(){
-    var bill=document.getElementById('bill_id').value;
-    var name=document.getElementById('Name').value;
-    var type=document.getElementById('type').value;
-    var bill_amt=document.getElementById('bill').value;
-
-    var cd = xmlDoc.createElement('cd');
-
-    var bill_id = xmlDoc.createElement('Car-Name');
-    newText1=xmlDoc.createTextNode(bill);
-    bill_id.appendChild(newText1);
-
-    var pname = xmlDoc.createElement('Car-Make');
-    newText2=xmlDoc.createTextNode(name);
-    pname.appendChild(newText2);
-
-    var ptype = xmlDoc.createElement('Car-Type');
-    newText3=xmlDoc.createTextNode(type);
-    ptype.appendChild(newText3);
-
-    var bill_amount = xmlDoc.createElement('Gear-Type');
-    newText4=xmlDoc.createTextNode(bill_amt);
-    bill_amount.appendChild(newText4);
-
-
-
-    cd.appendChild(bill_id);
-    cd.appendChild(pname);
-    cd.appendChild(ptype);
-    cd.appendChild(bill_amount);
-
-
-    var y = xmlDoc.getElementsByTagName("fleet");
-    y[0].appendChild(cd);
-    show(xmlDoc);
-    
-}
-
-var node2;
-function change(node1){
-    node2=node1;
-    y=xmlDoc.getElementsByTagName("cd")[node1];
-    document.getElementById('bill_id1').value=y.getElementsByTagName("Car-Name")[0].childNodes[0].nodeValue;
-    document.getElementById('Name1').value=y.getElementsByTagName("Car-Make")[0].childNodes[0].nodeValue;
-    document.getElementById('type1').value=y.getElementsByTagName("Car-Type")[0].childNodes[0].nodeValue;
-    document.getElementById('bill1').value=y.getElementsByTagName("Gear-Type")[0].childNodes[0].nodeValue;
-}
-
-function replace(){
-    y=xmlDoc.getElementsByTagName("cd")[node2];
-    y.getElementsByTagName("Car-Name")[0].childNodes[0].nodeValue=document.getElementById('bill_id1').value;
-    y.getElementsByTagName("Car-Make")[0].childNodes[0].nodeValue=document.getElementById('Name1').value;
-    y.getElementsByTagName("Car-Type")[0].childNodes[0].nodeValue=document.getElementById('type1').value;
-    y.getElementsByTagName("Gear-Type")[0].childNodes[0].nodeValue=document.getElementById('bill1').value;
-    show(xmlDoc);
-    $('#new2').modal('hide'); 
-}
